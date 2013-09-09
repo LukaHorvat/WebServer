d4a1a9de5b9fd11d08545f1c8f87332beb7c511c
Luka Horvat <lukahorvat9@gmail.com>
2013-09-09 09:20:59 +0200

    Init

---
+++ WebServer/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace WebServer
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var server = new HttpListener();
+			server.Prefixes.Add("http://+:8080/test/");
+			server.Stop();
+			server.Start();
+
+			server.BeginGetContext(req =>
+			{
+				var listener = (HttpListener)req.AsyncState;
+
+				var context = listener.EndGetContext(req);
+				var request = context.Request;
+				var response = context.Response;
+
+				string responseString = "<html><body> Hello world!</body></html>";
+				byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
+				response.ContentLength64 = buffer.Length;
+				var output = response.OutputStream;
+				output.Write(buffer, 0, buffer.Length);
+				// You must close the output stream.
+				output.Close();
+			}, server);
+
+			Console.ReadKey();
+		}
+	}
+}
